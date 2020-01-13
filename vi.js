10f565dc8c062b26d6ddcf02c0385a1262e2dca6
Prince Kumar <46297619+prince21298@users.noreply.github.com>
2020-01-13 17:24:24 +0530

    Update vi.js

--- vi.js
+++ vi.js
@@ -1,13 +1,39 @@
-var a=["abcd","abcde","abc","a","ab"];
-for (var i=0; i<a.length; i++){
-    for (var j=0; j<a.length-1; j++){
-        var f=a[j].length;
-        var g=a[j+1].length;
-        if (f<g){
-            var small=a[j];
-            a[j]=a[j+1];
-            a[j+1]=small;
-        }
-    }
-}
-console.log(a);
+// var a=["abcd","abcde","abc","a","ab"];
+// for (var i=0; i<a.length; i++){
+//     for (var j=0; j<a.length-1; j++){
+//         var f=a[j].length;
+//         var g=a[j+1].length;
+//         if (f<g){
+//             var small=a[j];
+//             a[j]=a[j+1];
+//             a[j+1]=small;
+//         }
+//     }
+// }
+// console.log(a);
+
+
+
+// #    PRIME NUMBER
+
+
+
+// var a = [2,3,4,5,6,7,9,11,12,13]
+// var b=a.map((data)=>{
+//     for (var i=2; i<=data; i++){
+//         if (i<data){
+//         	if(data%i == 0){
+//             break
+// 	        }
+//         }
+//         else{
+// 	        var d='true'
+// 	        break
+// 	        }
+//     }
+//     if (d=='true'){
+//         console.log(data);
+//         // return data;
+//     }
+// })
+// // console.log(b)        
